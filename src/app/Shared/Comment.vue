193c9430392baa02dc8836e900aebd94aa5226d5
Elgashia <erwinousy@gmail.com>
2018-11-17 18:48:42 +0900

    Add comment add & edit feature

--- src/app/Shared/Comment.vue
+++ src/app/Shared/Comment.vue
@@ -6,7 +6,8 @@
                     label="yourName:"
                     label-class="text-left"
                     label-for="userName">
-        <b-form-input class="ml-0" id="userName" v-model.trim="comment.username" :state="null" type="text" placeholder="username"></b-form-input>
+        <b-form-input class="ml-0" id="userName" v-model.trim="comment.username"
+                      :state="null" type="text" placeholder="username"></b-form-input>
       </b-form-group>
       <b-input-group>
         <b-form-input id="inputLive"
@@ -19,16 +20,25 @@
       </b-input-group>
     </b-card>
 
-    <b-card class="text-left mt-2 mb-2" :header="data.username ? data.username : ''" border-variant="info" v-for="(data, idx) in commentInfo" :key="idx">
-      <p class="card-text">
+    <b-card class="text-left mt-2 mb-2" :header="data.username ? data.username : ''" border-variant="info"
+            v-for="(data, idx) in commentInfo" :key="idx">
+      <p class="card-text" v-if="!data.state">
         {{ data.comment }}
       </p>
+      <b-form-input v-model="dumpcomment[idx]" type="text"
+                    placeholder="Enter Comment" v-if="data.state"></b-form-input>
+
+      <b-button v-if="!data.state" @click="tryEdit(idx)" text-variant="gray" size="sm" class="mt-1 mr-1">edit</b-button>
+      <b-button style="display:none;" v-if="!data.state" variant="info" size="sm" class="mt-1">delete</b-button>
+      <b-button v-if="data.state" @click="editComment(idx, dumpcomment[idx])" text-variant="gray" size="sm" class="mt-1 mr-1">Confirm</b-button>
+      <b-button v-if="data.state" @click="tryEdit(idx)" text-variant="gray" size="sm" class="mt-1">cansel</b-button>
     </b-card>
   </div>
 </template>
 
 <script>
-import { getComment, addComment } from '@/app/api/comment'
+import Vue from 'vue'
+import { getComment, addComment, editComment } from '@/app/api/comment'
 
 export default {
   data () {
@@ -38,7 +48,9 @@ export default {
         name: '',
         comment: ''
       },
-      commentInfo: []
+      commentInfo: [],
+      dumpcomment: [],
+      state: false
     }
   },
   methods: {
@@ -46,11 +58,32 @@ export default {
       const res = await addComment(comment)
       this.commentInfo = [ ...this.commentInfo, res ]
       return this.commentInfo
+    },
+    tryEdit (idx) {
+      var item = this.commentInfo[idx]
+      if (item) {
+        this.dumpcomment[idx] = item.comment
+        Vue.set(this.commentInfo, idx, { ...item, state: !item.state })
+      }
+    },
+    editComment (idx, dump) {
+      var item = this.commentInfo[idx]
+      editComment(item, dump).then(res => {
+        if (res) {
+          item.comment = dump
+        }
+        Vue.set(this.commentInfo, idx, { ...item, state: !item.state })
+      })
     }
   },
   created () {
     getComment(this.$route.params.id).then((res) => {
       this.commentInfo = res
+
+      this.commentInfo.forEach((data) => {
+        this.dumpcomment.push(data.comment)
+        data['state'] = false
+      })
     })
   }
 }
--- src/app/api/comment.js
+++ src/app/api/comment.js
@@ -1,44 +1,43 @@
 import localforage from 'localforage'
 
-const comment = [
-  {
-    cmtId: 1,
-    username: 'gentile',
-    pw: '1234',
-    comment: 'thk. new informations!',
-    postId: 1
-  },
-  {
-    cmtId: 2,
-    username: 'gentile',
-    pw: '1234',
-    comment: 'But are there any new news?',
-    postId: 1
-  },
-  {
-    cmtId: 3,
-    username: 'admin',
-    pw: '1234',
-    comment: 'test',
-    postId: 2
-  },
-  {
-    cmtId: 4,
-    username: 'gentile',
-    pw: '1234',
-    comment: 'test2',
-    postId: 2
-  }
-]
+// const comment = [
+//   {
+//     cmtId: 1,
+//     username: 'gentile',
+//     pw: '1234',
+//     comment: 'thk. new informations!',
+//     postId: 1
+//   },
+//   {
+//     cmtId: 2,
+//     username: 'gentile',
+//     pw: '1234',
+//     comment: 'But are there any new news?',
+//     postId: 1
+//   },
+//   {
+//     cmtId: 3,
+//     username: 'admin',
+//     pw: '1234',
+//     comment: 'test',
+//     postId: 2
+//   },
+//   {
+//     cmtId: 4,
+//     username: 'gentile',
+//     pw: '1234',
+//     comment: 'test2',
+//     postId: 2
+//   }
+// ]
 
 export const getComment = async (id) => {
   let tmp = []
   const result = []
 
-  for (let i = 0; i < comment.length; i++) {
-    await localforage.setItem('cmt-' + (i + 1), JSON.stringify(comment[i]))
-  }
-
+  // for (let i = 0; i < comment.length; i++) {
+  //   await localforage.setItem('cmt-' + (i + 1), JSON.stringify(comment[i]))
+  // }
   tmp = await localforage.startsWith('cmt-')
   Object.keys(tmp).forEach((key) => {
     var data = JSON.parse(tmp[key])
@@ -63,6 +62,19 @@ export const addComment = async (data) => {
 
     localforage.setItem('cmt-' + (cmtLen + 1), JSON.stringify(data))
     return data
+  } else {
+    alert('username, comment를 모두 입력해주셔야합니다.')
   }
   return false
+}
+
+export const editComment = async (data, dummyData) => {
+  if (data.comment !== dummyData) {
+    await localforage.setItem('cmt-' + data.cmtId, JSON.stringify({ ...data, comment: dummyData }))
+    alert('댓글이 수정되었습니다.')
+    return true
+  } else {
+    alert('fuck!')
+    return false
+  }
 }
